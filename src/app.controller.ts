c0cc5dcfe9780f0fe61a709399574d6c21b41d92
Omry Zuta <omry.zuta@riskifed.com>
2022-10-17 12:33:07 +0300

    eb alignment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,6 @@
 import {Controller, Get, HttpStatus} from '@nestjs/common';
-import {AppService} from './app.service';
-
 @Controller()
 export class AppController {
-
-
-    @Get()
-    index() {
-        return 'home page'
-    }
 
     @Get('health')
     healthcheck() {
